ec32a343ee415f6341cc86a25bd1dedf38813098
haselnuts <haselnuts@hotmail.com>
2021-01-10 19:02:00 +0000

    changed the entire buildQuiz again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 // Variables
-const quizContainer = document.getElementById("quiz");
+
 const submitBtn = document.getElementById("submit");
 const resultsContainer = document.getElementById("results");
 
 //All questions and answers go here
 const quizQuestions = [
-    {   quest: "How was the Dellendistrict created?",
+    {   question: "How was the Dellendistrict created?",
         answ: ["Meteor", "Volcano", "Big Bang", "Earthquake"], 
         correct: 0                   
     },
-    {   quest: "What is the stone called that only can be found in the Dellendistrict?",
+    {   question: "What is the stone called that only can be found in the Dellendistrict?",
         answ: [ "Dellenit", "Dellenkilt", "Dellenstone", "Dellenquartz"],
         correct: 0
     },
-    {   quest: "When were the Dellenlakes formed?",
+    {   question: "When were the Dellenlakes formed?",
         answ: ["Stone Age", "Bronce Age", "Ice Age", "Iron Age"],
         correct: 2
     },
-    {   quest: "Which is the biggest place in the Dellendistrict?",
+    {   question: "Which is the biggest place in the Dellendistrict?",
         answ: ["Bjuråker", "Norrbo", "Delsbo", "Moviken"],
         correct: 2
     },
-    {   quest: "Which place is not lying at the Dellenlakes?",
+    {   question: "Which place is not lying at the Dellenlakes?",
         answ: ["Delsbo", "Moviken", "Friggesund", "Eckelsbo"],
         correct: 3
     }
@@ -29,34 +29,32 @@ const quizQuestions = [
 
 //Build the Quiz
 function buildQuiz() {
-    //to store the HTML output
-    const output = [];
+    const quizContainer = document.getElementById("quiz");
 
     //for each question
-    quizQuestions.forEach(function (currentQuestion, answerQuestion) {
-        //to store answers
-        const answers = [];
+    let quizStr = "";
 
-        //for each answer
-        for(answer in currentQuestion.answers) {
-            answers.push(
-                //this is a template literal
-                `<label>
-                <input type="button" name="quest${answerQuestion}" value="answers">
-                ${currentQuestion.answers[answ]}
-                </label>`
-            );
-        };
+    quizQuestions.forEach( function (currentQuestion) {
 
-        output.push (
-            `<div class="question"> ${currentQuestion.quest}</div>
-            <div class="answers"> ${answers.join("")}</div>`
-    )
+        let answerStr ="";
+        currentQuestion.answ.forEach( function(answer){
+            answerStr +=
+            `<label>
+                <input type="button" name="" value="${answer}" class="answer">
+            </label>`
+        });
+
+        quizStr +=
+        `<div>
+            <h3 class="question">${currentQuestion.question}</h3>
+            ${answerStr}            
+        </div>`
     });
-    quizContainer.innerHTML = output.join("");    
+
+    quizContainer.innerHTML = quizStr;
+
 };
 
 
-function showResults() {};
- 
-submitBtn.addEventListener ("click", showResults);
+    buildQuiz();
+
