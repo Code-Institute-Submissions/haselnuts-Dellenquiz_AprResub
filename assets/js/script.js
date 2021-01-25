718134ceb99492c2a8ef09e5fc0e066ce93af84c
haselnuts <haselnuts@hotmail.com>
2021-01-25 12:07:55 +0000

    startGame function started, added showQuestions function, added eventListener - click, added const for  getElementById's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,54 @@
+//VARIABLES
+//Connects the elements from the index.html elements with the new variables created in the script.js
+const startButton = document.getElementById("start-btn");
+const quizContainerElement = document.getElementById("quiz-container");
+const questionElement = document.getElementById("question");
+const answerElement = docuemnt.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const previousButton = document.getElementById("previous-btn");
+const submitButton = document.getElementById("submit-btn")
+
+//EVENTLISTENER
+//Connects the EventListener with the functions
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", nextQuestion);
+previousButton.addEventListener("click", previousQuestion)
+submitButton.addEventListener("click", submitQuiz);
+
+
+//FUNCTIONS
+function startGame() {
+    //When start button pressed the start button will hide
+    startButton.classList.add("hide");
+    //After the start button is pressed, it will hide and the quiz container will show up
+    quizContainerElement.classList.remove("hide");
+    //Quiz starts with the first question in the quizQuestion array
+    currentQuestion = 0;
+    setNextQuestion
+};
+
+function showQuestions(question) {
+    questionElement.innerHTML = question.question;
+}
+
+showQuestions(currentQuestion)
+
+function selectAnswer() {
+    
+};
+
+function nextQuestion() {
+
+};
+
+function previousQuestion() {
+
+};
+
+function submitQuiz() {
+
+};
+
 //ALL QUESTIONS AND ANSWERS GO HERE
 const quizQuestions = [
     {   question: "How was the Dellendistrict created?",
@@ -21,36 +72,3 @@ const quizQuestions = [
         correct: 3
     }
 ];
-
-//VARIABLES
-const startButton = document.getElementById("start-btn");
-const nextButton = document.getElementById("next-btn");
-const previousButton = document.getElementById("previous-btn");
-const submitButton = document.getElementById("submit-btn");
-
-//EVENTLISTENER
-startButton.addEventListener("click", startGame);
-nextButton.addEventListener("click", nextQuestion);
-previousButton.addEventListener("click", previousQuestion);
-submitButton.addEventListener("click", submitQuiz);
-
-//FUNCTIONS
-function startGame() {
-    console.log("started");
-};
-
-function selectAnswer() {
-
-};
-
-function nextQuestion() {
-
-};
-
-function previousQuestion() {
-
-};
-
-function submitQuiz() {
-
-};
